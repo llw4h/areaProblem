082491d573ac96640d57654334ff9967194ab240
llw4h <30249831+llw4h@users.noreply.github.com>
2023-09-01 22:27:10 +0800

    added reverse function, separated calculation from checking of direction

--- areaProblem.java
+++ areaProblem.java
@@ -2,14 +2,26 @@ import java.util.ArrayList;
 
 public class areaProblem {
 
+    // checks if clockwise then reverse before calculation
     private static double calculateArea(double[] x, double[] y){
+
+        if (clockwise(x, y)) {
+            double[] reversedX = reverse(x, x.length);
+            double[] reversedY = reverse(y, y.length);
+            return areaFormula(reversedX, reversedY);
+        }
+        else {
+            return areaFormula(x, y);
+        }
+    }
+
+    private static double areaFormula(double[] x, double[] y){
         double area = 0;
         int size = x.length - 1;
     
         for (int i = 0; i < size; i++){
             area += (x[i] * y[i+1]) + (x[i+1] * y[i] * -1);
         }
-
         return area/2;
     }
 
@@ -23,14 +35,21 @@ public class areaProblem {
             return true;
         }
         return false;
-    } 
+    }
+
+    private static double[] reverse(double[] arr, int size){
+        double[] reverseArr = new double[size];
+        int reverseIndex = size;
+        for (int i = 0; i < size; i++) {
+            reverseArr[reverseIndex - 1] = arr[i];
+            reverseIndex -= 1;
+        }
+        return reverseArr;
+    }
 
     public static void main(String[] args) {
-        // unarranged given for example 1
-        double[] uX = {0.0, 0.0, 1.0, 1.0};
-        double[] uY = {0.0, 1.0, 1.0, 0.0}; 
-
-        // manually arranged given for example 1
+        
+        /* // manually arranged given for example 1
         double[] x = {0, 1, 1, 0};
         double[] y = {0, 0, 1, 1};
 
@@ -45,8 +64,24 @@ public class areaProblem {
         // manually arranged coordinates for example 4
         double[] x4 = {1, -1, -2, -2, 0, 1};
         double[] y4 = {1, 1, 0, -1, -2, -2};
+ */
 
-        //System.out.println(clockwise(uX, uY));
+        // example 1
+        double[] x = {0, 0, 1, 1};
+        double[] y = {0, 1, 1, 0}; 
+        
+        // example 2
+        double[] x2 = {0, 0, 1};
+        double[] y2 = {0, 1, 1};
+
+        // example 3
+        double[] x3 = {0, 3, 3, 4, 4, 2, 2, 1, 1, 0};
+        double[] y3 = {0, 0, 1, 1, 2, 2, 3, 3, 1, 1};
+
+        // example 4 - expected output 7.5
+        double[] x4 = {1, -1, -2, -2, 0, 1};
+        double[] y4 = {1, 1, 0, -1, -2, -2};
+
         System.out.println(calculateArea(x, y));
     }
 }
