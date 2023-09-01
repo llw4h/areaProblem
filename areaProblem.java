2de630ebddce850f91d8ce9b9277f241473382a2
llw4h <30249831+llw4h@users.noreply.github.com>
2023-09-01 22:04:36 +0800

    checker for clockwise

--- areaProblem.java
+++ areaProblem.java
@@ -4,43 +4,49 @@ public class areaProblem {
 
     private static double calculateArea(double[] x, double[] y){
         double area = 0;
-        // subtracts 1 from the array size to use for the formula
         int size = x.length - 1;
+    
         for (int i = 0; i < size; i++){
             area += (x[i] * y[i+1]) + (x[i+1] * y[i] * -1);
         }
+
         return area/2;
     }
 
-   /*  private static double[] counterClockwise(double[] x, double[] y){
-        // TODO 
-    } */
+    private static boolean clockwise(double[] x, double[] y){
+        double sum = 0;
+        int size = x.length - 1;
+        for (int i = 0; i < size; i++){
+            sum += (x[i+1] - x[i]) * (y[i+1] + y[i]);
+        }
+        if (sum > 0) {
+            return true;
+        }
+        return false;
+    } 
+
     public static void main(String[] args) {
         // unarranged given for example 1
-        /* double[] x = {0.0, 0.0, 1.0, 1.0};
-        double[] y = {0.0, 1.0, 1.0, 0.0}; */
+        double[] uX = {0.0, 0.0, 1.0, 1.0};
+        double[] uY = {0.0, 1.0, 1.0, 0.0}; 
 
         // manually arranged given for example 1
-        double[] x = {0.0, 1.0, 1.0, 0.0};
-        double[] y = {0.0, 0.0, 1.0, 1.0};
+        double[] x = {0, 1, 1, 0};
+        double[] y = {0, 0, 1, 1};
 
         // manually arranged coordinates for example 2
-        double[] x2 = {0.0, 1.0, 0.0};
-        double[] y2 = {0.0, 1.0, 1.0};
+        double[] x2 = {0, 1, 0};
+        double[] y2 = {0, 1, 1};
 
         // manually arranged coordinates for example 3
         double[] x3 = {0, 3, 3, 4, 4, 2, 2, 1, 1, 0};
         double[] y3 = {0, 0, 1, 1, 2, 2, 3, 3, 1, 1};
 
         // manually arranged coordinates for example 4
-        double[] x4 = {0, 1, 1, -1, -2, -2};
-        double[] y4 = {-2, -2, 1, 1, 0, -1};
+        double[] x4 = {1, -1, -2, -2, 0, 1};
+        double[] y4 = {1, 1, 0, -1, -2, -2};
 
-        // unarranged coordinates for example 4
-        double[] uX4 = {1, -1, -2, -2, 0, 1};
-        double[] uY4 = {1, 1, 0, -1, -2, -2};
-
-
+        //System.out.println(clockwise(uX, uY));
         System.out.println(calculateArea(x, y));
     }
 }
